4115bed64066a544724a7bee52322a06efb4c1a7
Simeon Ivanov <simeonivanov7@gmail.com>
2018-11-20 00:24:19 +0300

    Adapting code (reforming content storage and IMG-parsing) and SQL-template to better view

--- parser.py
+++ parser.py
@@ -29,7 +29,7 @@ def main(category_href): # Parse chosen category
 	post_global_counter = 970
 
 	global sql_global
-	sql_global = codecs.open("sql_global.sql","a+", "utf-8")
+	sql_global = codecs.open("sql_global.sql", "w+", "utf-8")
 
 	for html_doc_inside_href in array_hrefs:
 		post_global_counter += 1
@@ -42,23 +42,37 @@ def parse_inside_page(html_doc_inside_href, img_global_counter): # Parse particu
 	html_doc_inside = urllib2.urlopen(html_doc_inside_href)
 	soup_inside = BeautifulSoup(html_doc_inside, "html.parser")
 
-	title = soup_inside.select_one('.Titulo').get_text() # class can be changed to those which contain Main Heading/title of the page
-	title_translaterate = translaterate(title)
-
-	content_text = try_except('.objectosSubBlock', soup_inside) + "\n" + try_except('.text_body', soup_inside) # classes can be changed to those which contain text content
+	content_div = soup_inside.select_one('.Content')
 
 	img_counter = 0
 	img_array = []
+	href_array = []
+	text_array = []
 
-	content_div = soup_inside.select_one('.Content')
+	title = soup_inside.select_one('.Titulo').get_text() # class can be changed to those which contain Main Heading/title of the page
+	title_translaterate = translaterate(title)
+
+	excerpt = clear_white_space(try_except('.objectosSubBlock', soup_inside), "\s", 3)
+	main_text = clear_white_space(try_except('.text_body', soup_inside), "\s", 3)
+
+	print(html_doc_inside_href) # Catching possible errors
+
+	main_img_node = content_div.select_one('img')
+
+	try: # trying to parse main image's srcset || src
+		href_array.append((str(main_img_node.get('srcset')).split()[-2]))
+	except:
+		href_array.append(main_img_node.get('src'))
+
+	href_array.extend(list(map(lambda x: x.get('href'), content_div.select('.text_body a[href^="https://nn-vent.ru/wp-content/uploads/"]'))))
 	
-	for link in content_div.find_all('img'):
-		href = u"{}".format(link.get('src'))
+	for href in href_array:
 
 		img_counter += 1
 
 		img_global_counter += 1
 		img_array.append(img_global_counter)
+		
 
 		try:
 			save_in_folder(post_global_counter, img_global_counter, href, title_translaterate, title_translaterate, img_counter)
@@ -70,7 +84,7 @@ def parse_inside_page(html_doc_inside_href, img_global_counter): # Parse particu
 			continue
 
 
-	sql_global.write(sql_template(replace_single_quote(clear_white_space(title, '\n', 0)), title_translaterate, post_global_counter, img_array, replace_single_quote(clear_white_space(content_text, '\s', 3))))
+	sql_global.write(sql_template(excerpt, main_text, replace_single_quote(clear_white_space(title, '\n', 0)), title_translaterate, post_global_counter, img_array))
 
 	return img_counter
 
@@ -90,7 +104,7 @@ def clear_text(text): # Clear text from messy symbols
 	return text
 
 
-def clear_white_space(text, method, num_of_space): # Clear white space 
+def clear_white_space(text, method="\s", num_of_space=0): # Clear white space 
 	reg = re.compile(r"(?m)%s{%s,}" % (method, num_of_space))
 	text = reg.sub('', text)
 	return text
@@ -112,16 +126,16 @@ def replace_single_quote(text): # Avoiding SQL single quote INSERT error
 
 
 def save_in_folder(post_global_counter, img_global_counter, file_link, folder_name, file_name, img_counter): # Save img in page related folder
-	filename = 'c:/users/симеон/desktop/py_parser/{}-{}/{}-{}-{}.jpg'.format(post_global_counter, folder_name, img_global_counter, file_name, img_counter)
-	folder_name = os.path.dirname(filename)
-	if not os.path.exists(folder_name):
-		os.makedirs(folder_name)
+	# filename = 'c:/users/симеон/desktop/py_parser/{}-{}/{}-{}-{}.jpg'.format(post_global_counter, folder_name, img_global_counter, file_name, img_counter)
+	# folder_name = os.path.dirname(filename)
+	# if not os.path.exists(folder_name):
+	# 	os.makedirs(folder_name)
 
-	urllib2.urlretrieve(file_link, filename)
-	save_global(img_global_counter, file_link, file_name, img_counter)
+	# urllib2.urlretrieve(file_link, filename)
+	save_in_global_folder(img_global_counter, file_link, file_name, img_counter)
 
 
-def save_global(img_global_counter, file_link, file_name, img_counter): # Save img in stock folder
+def save_in_global_folder(img_global_counter, file_link, file_name, img_counter): # Save img in stock folder
 	filename = 'c:/users/симеон/desktop/py_parser/1-ALL-IMG/{}-{}-{}.jpg'.format(img_global_counter, file_name,	img_counter)
 	folder_name = os.path.dirname(filename)
 	if not os.path.exists(folder_name):
@@ -130,39 +144,15 @@ def save_global(img_global_counter, file_link, file_name, img_counter): # Save i
 	urllib2.urlretrieve(file_link, filename)
 
 
-def sql_template(post_title, post_name, post_global_counter, img_array, content_text): # building SQL-request for new page
+def sql_template(excerpt, main_text, post_title, post_name, post_global_counter, img_array): # building SQL-request for new page
 	
 	# Create content with right amount if images
-	content_inner = """
-	[vc_row]
-		[vc_column]
-			[vc_row_inner]
-				[vc_column_inner width="1/2"]
-					[vc_single_image image="{}" img_size="full" alignment="center" onclick="link_image"]
-				[/vc_column_inner]
-				[vc_column_inner width="1/2"]
-					[vc_column_text]
-						{}
-					[/vc_column_text]
-				[/vc_column_inner]
-			[/vc_row_inner]
-			[vc_empty_space height="40px"]
-			[vc_custom_heading text="ВЫПОЛНЕННЫЕ РАБОТЫ" font_container="tag:h2|text_align:left|color:%230079ce" use_theme_fonts="yes" css=".vc_custom_1538483422586{{border-left-width: 3px !important;padding-left: 15px !important;border-left-color: #0079ce !important;border-left-style: solid !important;}}"]
-			[vc_empty_space height="35px"]
-			[ultimate_carousel slides_on_desk="2" slides_on_tabs="2" slides_on_mob="1" autoplay="off" next_icon="ultsl-arrow-right6" prev_icon="ultsl-arrow-left6" dots_icon="ultsl-radio-unchecked"]
-	""".format(img_array[0], content_text)
+	content_inner = """[vc_row][vc_column][vc_row_inner][vc_column_inner width="1/2"][vc_single_image image="{}" img_size="full" alignment="center" onclick="link_image"][/vc_column_inner][vc_column_inner width="1/2"][vc_column_text]{}[/vc_column_text][/vc_column_inner][/vc_row_inner][vc_empty_space height="40px"][vc_column_text]{}[/vc_column_text][vc_empty_space height="40px"][vc_custom_heading text="ВЫПОЛНЕННЫЕ РАБОТЫ" font_container="tag:h2|text_align:left|color:%230079ce" use_theme_fonts="yes" css=".vc_custom_1538483422586{{border-left-width: 3px !important;padding-left: 15px !important;border-left-color: #0079ce !important;border-left-style: solid !important;}}"][vc_empty_space height="35px"][ultimate_carousel slides_on_desk="2" slides_on_tabs="2" slides_on_mob="1" autoplay="off" next_icon="ultsl-arrow-right6" prev_icon="ultsl-arrow-left6" dots_icon="ultsl-radio-unchecked"]""".format(img_array[0], excerpt, main_text)
 
 	for img_id in img_array[1:]:
-		content_inner += """
-					[vc_single_image image="{}" img_size="full" alignment="center" onclick="link_image"]
-		""".format(img_id)
+		content_inner += """[vc_single_image image="{}" img_size="full" alignment="center" onclick="link_image"]""".format(img_id)
 
-	content_inner += """
-			[/ultimate_carousel]
-			[vc_empty_space height="15px"]
-		[/vc_column]
-	[/vc_row]
-	"""
+	content_inner += """[/ultimate_carousel][vc_empty_space height="15px"][/vc_column][/vc_row]"""
 
 	# Create SQL-request with all variables (to wptg_posts and wptg_term_relationships)
 	sql_request = """
@@ -177,23 +167,26 @@ def sql_template(post_title, post_name, post_global_counter, img_array, content_
 							comment_status,
 							ping_status,
 							post_type,
+							post_excerpt,
 							post_content) 
 	VALUES (1,
-			'2018-11-18', 
-			'2018-11-18', 
-			'2018-11-18', 
-			'2018-11-18', 
+			'2018-11-19', 
+			'2018-11-19', 
+			'2018-11-19', 
+			'2018-11-19', 
 			'{}',
 			'publish', 
 			'{}',
 			'closed',
 			'closed',
 			'post',
+			'{}',
 			'{}');
-	""".format(post_title, post_name, content_inner)
+
+	""".format(post_title, post_name, excerpt[:200]+"...", content_inner)
 
 	return sql_request
 
 
 if __name__ == '__main__':
-	main(str(input("Write in site's categoty link: "))) # link of sites categoty page
+	main(str(input("Type link: "))) # link of sites categoty page (https://nn-vent.ru/objects/nashi-klientyi/)
